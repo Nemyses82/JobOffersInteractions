93c3df69ce92e35582c65db555ad71c15fa600ab
Daniele Giometti <giome82@gmail.com>
2021-10-23 22:15:22 +0100

    creation interactions mode 2

--- JobOffersInteractions/Program.cs
+++ JobOffersInteractions/Program.cs
@@ -34,9 +34,6 @@ namespace JobOffersInteractions
                 var grouped = interactions.GroupBy(a => a.UserId)
                     .Select(g => new { userId = g.Key, Count = g.Count() }).OrderBy(x => x.userId).ToList();
 
-                // var itemInGroup = grouped.FirstOrDefault(x => x.userId == randomUserId);
-
-                // fill interactions with 25 unique items
                 if (interactions.Count < numberOfUniqueUsers && interactions.Count(x => x.UserId == randomUserId) == 0)
                 {
                     interactions.Add(new Interaction
@@ -47,114 +44,41 @@ namespace JobOffersInteractions
                     continue;
                 }
 
-                // ensure I have at least 25 unique items (regardless number of interactions)
-                // if (interactions.Count >= numberOfUniqueUsers)
-                // {
-                //     // look up for already existing item and ensure they have at least 2 interactions
-                //     if (itemInGroup != null && itemInGroup.Count < numberOfMinimumInteractions)
-                //     {
-                //         interactions.Add(new Interaction
-                //         {
-                //             UserId = randomUserId,
-                //             RandomJobOfferId = randomJobOfferId
-                //         });
-                //         continue;
-                //     }
-                //     
-                //     // ensure I have 25 unique items
-                //     if (grouped.Count(x => x.Count >= numberOfMinimumInteractions) >= numberOfUniqueUsers)
-                //     {
+                var allWithTwoInteractionGrouped = interactions
+                    .GroupBy(a => a.UserId)
+                    .Select(g => new { userId = g.Key, Count = g.Count() })
+                    .Where(x => x.Count >= numberOfMinimumInteractions).ToList();
 
-                        var allWithTwoInteractionGrouped = interactions
-                            .GroupBy(a => a.UserId)
-                            .Select(g => new { userId = g.Key, Count = g.Count() })
-                            .Where(x => x.Count >= numberOfMinimumInteractions).ToList();
+                var allWithTwoInteraction = interactions.Where(x =>
+                    allWithTwoInteractionGrouped.Select(y => y.userId).Contains(x.UserId)).ToList();
 
-                        var allWithTwoInteraction = interactions.Where(x =>
-                            allWithTwoInteractionGrouped.Select(y => y.userId).Contains(x.UserId)).ToList();
+                var allWithOneInteraction = interactions.Where(x =>
+                    !allWithTwoInteractionGrouped.Select(y => y.userId).Contains(x.UserId)).ToList();
 
-                        var allWithOneInteraction = interactions.Where(x =>
-                            !allWithTwoInteractionGrouped.Select(y => y.userId).Contains(x.UserId)).ToList();
+                var oneInteractionMargin = allWithOneInteraction.Count * 2;
 
-                        var oneInteractionMargin = allWithOneInteraction.Count * 2;
+                var marginForMinimumInteractions = numberOfInteractions - (allWithTwoInteraction.Count + oneInteractionMargin);
 
-                        var marginForMinimumInteractions = numberOfInteractions - (allWithTwoInteraction.Count + oneInteractionMargin);
-
-                        if (marginForMinimumInteractions == 0)
-                        {
-                            var itemInGroup = grouped.FirstOrDefault(x => x.userId == randomUserId && x.Count == 1);
-                            if (itemInGroup != null)
-                            {
-                                interactions.Add(new Interaction
-                                {
-                                    UserId = randomUserId,
-                                    RandomJobOfferId = randomJobOfferId
-                                });
-                            }
-                            continue;
-                        }
-
+                if (marginForMinimumInteractions == 0)
+                {
+                    var itemInGroup = grouped.FirstOrDefault(x => x.userId == randomUserId && x.Count == 1);
+                    if (itemInGroup != null)
+                    {
                         interactions.Add(new Interaction
                         {
                             UserId = randomUserId,
                             RandomJobOfferId = randomJobOfferId
                         });
-                //     }
-                //
-                // }
+                    }
+                    continue;
+                }
+
+                interactions.Add(new Interaction
+                {
+                    UserId = randomUserId,
+                    RandomJobOfferId = randomJobOfferId
+                });
             }
-
-            //
-            // Solution that works but I may encounter case when item has only one interaction
-            //
-            // while (interactions.Count < numberOfInteractions)
-            // {
-            //     var randomUserId = random.Next(10, 100);
-            //     var randomJobOfferId = random.Next(30, 40);
-            //
-            //     var grouped = interactions.GroupBy(a => a.UserId)
-            //         .Select(g => new { userId = g.Key, Count = g.Count() }).ToList();
-            //
-            //     var itemInGroup = grouped.FirstOrDefault(x => x.userId == randomUserId);
-            //
-            //     // fill interactions with 25 unique items
-            //     if (interactions.Count < numberOfUniqueUsers && interactions.Count(x => x.UserId == randomUserId) == 0)
-            //     {
-            //         interactions.Add(new Interaction
-            //         {
-            //             UserId = randomUserId,
-            //             RandomJobOfferId = randomJobOfferId
-            //         });
-            //         continue;
-            //     }
-            //
-            //     // ensure I have at least 25 unique items (regardless number of interactions)
-            //     if (interactions.Count >= numberOfUniqueUsers)
-            //     {
-            //         // look up for already existing item and ensure they have at least 2 interactions
-            //         if (itemInGroup != null && itemInGroup.Count < numberOfMinimumInteractions)
-            //         {
-            //             interactions.Add(new Interaction
-
-            //             {
-            //                 UserId = randomUserId,
-            //                 RandomJobOfferId = randomJobOfferId
-            //             });
-            //             continue;
-            //         }
-            //
-            //         // ensure I have 25 unique items
-            //         if (grouped.Count(x => x.Count >= numberOfMinimumInteractions) >= numberOfUniqueUsers)
-            //         {
-            //             interactions.Add(new Interaction
-            //             {
-            //                 UserId = randomUserId,
-            //                 RandomJobOfferId = randomJobOfferId
-            //             });
-            //         }
-            //
-            //     }
-            // }
 
             interactions = interactions.OrderBy(x => x.UserId).ToList();
             var groupedInteractions = interactions.GroupBy(a => a.UserId)
