d4dea8e5b4654c2d6e6093fe13fff81605e4661a
Daniele Giometti <giome82@gmail.com>
2022-02-08 23:52:26 +0000

    added interactions, joboffers and updated git ignore file

--- Models/Interaction.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace JobOffersInteractions.Models
-{
-    public record Interaction(int UserId, int JobId, long EpochTime)
-    {
-        public int UserId { get; set; } = UserId;
-        public int JobId { get; } = JobId;
-        public long EpochTime { get; } = EpochTime;
-    }
-}
